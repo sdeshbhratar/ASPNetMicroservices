eadc35a51b9c9def1a49f269027aa3c22d125b51
Sumant Deshbhratar <sumant.deshbhratar@hotmail.com>
2023-06-30 16:16:37 -0400

    Updated Basket API to call DIscount.Grpc service to get discount data

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,25 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountprotoservice;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountprotoservice)
+        {
+            _discountprotoservice = discountprotoservice ?? throw new ArgumentNullException(nameof(discountprotoservice));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountprotoservice.GetDiscountAsync(discountRequest);
+        }
+    }
+}
